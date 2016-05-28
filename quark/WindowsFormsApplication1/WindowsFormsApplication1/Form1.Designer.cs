ba3afd7b0b17f9fdfa7812f0849dd87332bb8ff0
wafflepandasz <wafflepandasz@gmail.com>
2016-05-28 14:30:06 -0400

    Add files via upload

---
+++ quark/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Quark
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Quark));
+            this.Open = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Open
+            // 
+            this.Open.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Open.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Open.FlatAppearance.BorderSize = 0;
+            this.Open.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Open.ForeColor = System.Drawing.Color.White;
+            this.Open.Location = new System.Drawing.Point(12, 12);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(51, 23);
+            this.Open.TabIndex = 2;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = false;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Save
+            // 
+            this.Save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Save.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Save.FlatAppearance.BorderSize = 0;
+            this.Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Save.Location = new System.Drawing.Point(59, 12);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(47, 23);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = false;
+            this.Save.Click += new System.EventHandler(this.Save_Click_1);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.AcceptsTab = true;
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Font = new System.Drawing.Font("Josefin Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.ForeColor = System.Drawing.Color.White;
+            this.richTextBox2.Location = new System.Drawing.Point(12, 34);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(532, 350);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // Quark
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Grip;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(556, 396);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Open);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Quark";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Quark";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ quark/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Quark : Form
+    {
+        public Quark()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (System.IO.StreamWriter sr = new System.IO.StreamWriter(sfd.FileName))
+                {
+                    foreach (var line in richTextBox2.Lines)
+                    {
+                        sr.WriteLine(line);
+                    }
+                }
+            }
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(ofd.FileName))
+                {
+                    richTextBox2.Text = sr.ReadToEnd();
+                }
+            }
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            // getting keywords/functions
+            string keywords = @"\b(public|private|partial|static|namespace|class|using|void|foreach|in|var|function|if|for|while)\b";
+            MatchCollection keywordMatches = Regex.Matches(richTextBox2.Text, keywords);
+
+            // getting types/classes from the text 
+            string types = @"\b(Console)\b";
+            MatchCollection typeMatches = Regex.Matches(richTextBox2.Text, types);
+
+            // getting comments (inline or multiline)
+            string comments = @"(\/\/.+?$|\/\*.+?\*\/)";
+            MatchCollection commentMatches = Regex.Matches(richTextBox2.Text, comments, RegexOptions.Multiline);
+
+            // getting strings
+            string strings = "\".+?\"";
+            MatchCollection stringMatches = Regex.Matches(richTextBox2.Text, strings);
+
+
+            // saving the original caret position + forecolor
+            int originalIndex = richTextBox2.SelectionStart;
+            int originalLength = richTextBox2.SelectionLength;
+            Color originalColor = Color.White;
+
+            // MANDATORY - focuses a label before highlighting (avoids blinking)
+            //titleLabel.Focus();
+
+            // removes any previous highlighting (so modified words won't remain highlighted)
+            richTextBox2.SelectionStart = 0;
+            richTextBox2.SelectionLength = richTextBox2.Text.Length;
+            richTextBox2.SelectionColor = originalColor;
+
+
+            // scanning...
+            foreach (Match m in keywordMatches)
+            {
+                richTextBox2.SelectionStart = m.Index;
+                richTextBox2.SelectionLength = m.Length;
+                richTextBox2.SelectionColor = Color.DeepPink;
+            }
+
+
+            foreach (Match m in typeMatches)
+            {
+                richTextBox2.SelectionStart = m.Index;
+                richTextBox2.SelectionLength = m.Length;
+                richTextBox2.SelectionColor = Color.DarkCyan;
+            }
+
+            foreach (Match m in commentMatches)
+            {
+                richTextBox2.SelectionStart = m.Index;
+                richTextBox2.SelectionLength = m.Length;
+                richTextBox2.SelectionColor = Color.Green;
+            }
+
+            foreach (Match m in stringMatches)
+            {
+                richTextBox2.SelectionStart = m.Index;
+                richTextBox2.SelectionLength = m.Length;
+                richTextBox2.SelectionColor = Color.DarkViolet;
+            }
+
+            // restoring the original colors, for further writing
+            richTextBox2.SelectionStart = originalIndex;
+            richTextBox2.SelectionLength = originalLength;
+            richTextBox2.SelectionColor = originalColor;
+
+            // giving back the focus
+            richTextBox2.Focus();
+        }
+    
+
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Save_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (System.IO.StreamWriter sr = new System.IO.StreamWriter(sfd.FileName))
+                {
+                    foreach (var line in richTextBox2.Lines)
+                    {
+                        sr.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
